efd5ed5b2843ad4d5b6e268c5b7c106b0f7a1542
Leandro Andrade <andradeleandro.contato@gmail.com>
2023-05-24 16:01:58 -0300

    Segundo Commit com exercício 4 corrigido, sem a lógica repetida

--- src/atividadeVarOpe/Exercicio4.java
+++ src/atividadeVarOpe/Exercicio4.java
@@ -28,20 +28,20 @@ public class Exercicio4 {
 		diferenca = ((numero1*numero2)-(numero3*numero4));
 		System.out.printf("Essa é a diferença: %.1f", diferenca);
 		
-		System.out.println("\nDigite o número 1:");
-		numero1 = leia.nextFloat();
-		
-		System.out.println("Digite o número 2:");
-		numero2 = leia.nextFloat();
-		
-		System.out.println("Digite o número 3:");
-		numero3 = leia.nextFloat();
-		
-		System.out.println("Digite o número 4:");
-		numero4 = leia.nextFloat();
-		
-		diferenca = ((numero1*numero2)-(numero3*numero4));
-		System.out.printf("Essa é a diferença: %.1f", diferenca);
+//		System.out.println("\nDigite o número 1:");
+//		numero1 = leia.nextFloat();
+//		
+//		System.out.println("Digite o número 2:");
+//		numero2 = leia.nextFloat();
+//		
+//		System.out.println("Digite o número 3:");
+//		numero3 = leia.nextFloat();
+//		
+//		System.out.println("Digite o número 4:");
+//		numero4 = leia.nextFloat();
+//		
+//		diferenca = ((numero1*numero2)-(numero3*numero4));
+//		System.out.printf("Essa é a diferença: %.1f", diferenca);
 		
 	}
 
